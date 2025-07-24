239bf817aa207067416c6f0c1cad8bb8184ca668
Pete Gadomski <pete.gadomski@gmail.com>
2025-07-24 15:02:50 -0600

    --wip-- [skip ci]

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,3 +1,4 @@
+import { useBreakpointValue } from "@chakra-ui/react";
 import { Layer, type DeckProps } from "@deck.gl/core";
 import { GeoJsonLayer } from "@deck.gl/layers";
 import { MapboxOverlay } from "@deck.gl/mapbox";
@@ -62,6 +63,22 @@ export default function Map() {
   const [bbox, setBbbox] = useState<BBox>();
   const [stacGeoparquetLayer, setStacGeoparquetLayer] =
     useState<GeoArrowPolygonLayer>();
+  const padding = useBreakpointValue({
+    base: {
+      top: window.innerHeight / 10,
+      bottom: window.innerHeight / 20,
+      right: window.innerWidth / 20,
+      // TODO fix for smaller viewport
+      left: window.innerWidth / 20 + window.innerWidth / 3,
+    },
+    md: {
+      top: window.innerHeight / 10,
+      bottom: window.innerHeight / 20,
+      right: window.innerWidth / 20,
+      // TODO fix for smaller viewport
+      left: window.innerWidth / 20 + window.innerWidth / 3,
+    },
+  });
 
   useEffect(() => {
     switch (value?.type) {
