b23cd419efa9c86597cff3b86a2921ce2e787ab1
Pete Gadomski <pete.gadomski@gmail.com>
2025-07-25 10:19:51 -0600

    feat: put the type in the example

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,38 @@
-import { Box, Button, HStack, Input, Menu, Portal } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  Button,
+  HStack,
+  Input,
+  Menu,
+  Portal,
+  Span,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import useStacMap from "../hooks/stac-map";
 import { ColorModeButton } from "./ui/color-mode";
 
 const EXAMPLES = [
-  ["eoAPI DevSeed", "https://stac.eoapi.dev/"],
+  ["eoAPI DevSeed", "API", "https://stac.eoapi.dev/"],
   [
     "Microsoft Planetary Computer",
+    "API",
     "https://planetarycomputer.microsoft.com/api/stac/v1",
   ],
-  ["Earth Search by Element 84", "https://earth-search.aws.element84.com/v1"],
-  ["NASA VEDA", "https://openveda.cloud/api/stac"],
+  [
+    "Earth Search by Element 84",
+    "API",
+    "https://earth-search.aws.element84.com/v1",
+  ],
+  ["NASA VEDA", "API", "https://openveda.cloud/api/stac"],
   [
     "Colorado NAIP",
+    "stac-geoparquet",
     "https://raw.githubusercontent.com/developmentseed/labs-375-stac-geoparquet-backend/refs/heads/main/data/naip.parquet",
   ],
   [
     "Simple item",
+    "item",
     "https://raw.githubusercontent.com/radiantearth/stac-spec/refs/heads/master/examples/simple-item.json",
   ],
 ];
@@ -73,9 +89,11 @@ function Examples() {
       <Portal>
         <Menu.Positioner>
           <Menu.Content>
-            {EXAMPLES.map(([text, href], index) => (
+            {EXAMPLES.map(([text, badge, href], index) => (
               <Menu.Item key={"example-" + index} value={href}>
                 {text}
+                <Span flex={1}></Span>
+                <Badge>{badge}</Badge>
               </Menu.Item>
             ))}
           </Menu.Content>
